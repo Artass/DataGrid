db184835bf414f58146a7e07b3bd637fe38fc045
Artem <52921771+Artass@users.noreply.github.com>
2019-07-31 10:57:41 +0300

    Add files via upload

---
+++ Таблица/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Таблица
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Tb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new Person { Title = "Test1", Nomer=1};
+            tb.Items.Add(data);
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            tb.Items.RemoveAt(tb.SelectedIndex);
+        }
+        
+        
+    }
+}
---
+++ Таблица/person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Таблица
+{
+    class Person
+    {
+        public string Title { get; set; }
+        public int Nomer { get; set; }
+    }
+}
